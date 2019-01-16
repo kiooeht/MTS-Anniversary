203783f0e9d3eee3b21e0de1f013456eaa42f8bc
Kiooeht <kiooeht@gmail.com>
2019-01-15 23:01:08 -0800

    Basic main mod class set up

---
+++ src/main/java/theAct/TheActMod.java
@@ -0,0 +1,78 @@
+package theAct;
+
+import basemod.BaseMod;
+import basemod.ModPanel;
+import basemod.interfaces.EditKeywordsSubscriber;
+import basemod.interfaces.EditStringsSubscriber;
+import basemod.interfaces.PostInitializeSubscriber;
+import com.badlogic.gdx.Gdx;
+import com.evacipated.cardcrawl.mod.stslib.Keyword;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.google.gson.Gson;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.EventStrings;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.nio.charset.StandardCharsets;
+
+@SpireInitializer
+public class TheActMod implements
+        PostInitializeSubscriber,
+        EditKeywordsSubscriber,
+        EditStringsSubscriber
+{
+    public static final Logger logger = LogManager.getLogger(TheActMod.class.getSimpleName());
+
+    public static void initialize()
+    {
+        BaseMod.subscribe(new TheActMod());
+    }
+
+    public static String assetPath(String path)
+    {
+        return "theActAssets/" + path;
+    }
+
+    @Override
+    public void receivePostInitialize()
+    {
+        // Settings panel
+        ModPanel settingsPanel = new ModPanel();
+
+
+        BaseMod.registerModBadge(ImageMaster.loadImage(assetPath("modBadge.png")), "MTS Anniversary Act", "Everyone", "TODO", settingsPanel);
+
+        // Add events here
+
+        // Add monsters here
+
+    }
+
+    @Override
+    public void receiveEditKeywords()
+    {
+        String language = "eng";
+        // TODO: Language support
+
+        Gson gson = new Gson();
+        String json = Gdx.files.internal(assetPath("localization/" + language + "/keywords.json")).readString(String.valueOf(StandardCharsets.UTF_8));
+        Keyword[] keywords = gson.fromJson(json, Keyword[].class);
+
+        if (keywords != null) {
+            for (Keyword keyword : keywords) {
+                BaseMod.addKeyword(keyword.PROPER_NAME, keyword.NAMES, keyword.DESCRIPTION);
+            }
+        }
+    }
+
+    @Override
+    public void receiveEditStrings()
+    {
+        String language = "eng";
+        // TODO: Language support
+        String path = "localization/" + language + "/";
+
+        BaseMod.loadCustomStringsFile(EventStrings.class, assetPath(path + "events.json"));
+    }
+}
