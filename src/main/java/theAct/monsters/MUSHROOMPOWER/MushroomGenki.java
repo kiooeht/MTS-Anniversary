1f009dec379b835ec4961f8cdd61a015bd22ab67
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-19 21:48:44 -0800

    nerf mushroom hp

--- src/main/java/theAct/monsters/MUSHROOMPOWER/MushroomGenki.java
+++ src/main/java/theAct/monsters/MUSHROOMPOWER/MushroomGenki.java
@@ -18,8 +18,8 @@ public class MushroomGenki extends AbstractMonster {
     public static final String ENCOUNTER_ID = TheActMod.makeID("MUSHROOMGANG");
     private static final MonsterStrings monsterStrings = CardCrawlGame.languagePack.getMonsterStrings(ID);
     public static final String NAME = monsterStrings.NAME;
-    private static final int MIN_HP = 23;
-    private static final int MAX_HP = 29;
+    private static final int MIN_HP = 18;
+    private static final int MAX_HP = 23;
     private static final int ASC_HP_MODIFIER = 4;
     private static final int STR_AMT = 1;
     private static final int STR_AMT_ASC_MODIFIER = 1;
