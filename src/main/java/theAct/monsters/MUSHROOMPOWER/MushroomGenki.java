c380eaeba121614ba46b1ae8dc4b10401d48b7a2
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-20 03:31:50 -0800

    Genki Mushroom anims

--- src/main/java/theAct/monsters/MUSHROOMPOWER/MushroomGenki.java
+++ src/main/java/theAct/monsters/MUSHROOMPOWER/MushroomGenki.java
@@ -30,7 +30,9 @@ public class MushroomGenki extends AbstractMonster {
 
     public MushroomGenki(float x, float y) {
         super(NAME, ID, MAX_HP, 0.0F, 10.0F, 280.0F, 280.0F, null, x, y);
-        this.img = ImageMaster.loadImage(TheActMod.assetPath("/images/monsters/phrog/temp.png"));
+        loadAnimation(TheActMod.assetPath("images/monsters/MUSHROOMPOWER/Genki.atlas"), TheActMod.assetPath("images/Genki.json"), 1.0F);
+        state.setAnimation(0, "Idle", true);
+        stateData.setMix("Idle", "Floop", 0.2F);
         if (AbstractDungeon.ascensionLevel >= 17) {
             strAmt = STR_AMT + STR_AMT_ASC_MODIFIER;
         } else {
@@ -64,6 +66,8 @@ public class MushroomGenki extends AbstractMonster {
             case 1:
                 for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
                     if (m != this) {
+                        state.setAnimation(0, "Floop", false);
+                        state.addAnimation(0, "Idle", true, 0.0F);
                         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, this, new StrengthPower(m, strAmt), strAmt));
                     }
                 }
