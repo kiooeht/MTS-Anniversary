4881a6f10f121d62651d06a0c71f88b8fc676a1b
Kyloman <kyloman@gmail.com>
2019-01-21 05:39:18 -0800

    finishing the event

--- src/main/java/theAct/events/ShamanCauldron.java
+++ src/main/java/theAct/events/ShamanCauldron.java
@@ -12,10 +12,12 @@ import com.megacrit.cardcrawl.events.AbstractImageEvent;
 import com.megacrit.cardcrawl.helpers.ScreenShake;
 import com.megacrit.cardcrawl.localization.EventStrings;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
+import com.megacrit.cardcrawl.potions.PotionSlot;
 import com.megacrit.cardcrawl.vfx.RainingGoldEffect;
 import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
 import theAct.TheActMod;
 import theAct.patches.PotionRarityEnum;
+import theAct.potions.*;
 import theAct.relics.PaperFaux;
 
 import static com.megacrit.cardcrawl.dungeons.AbstractDungeon.returnRandomPotion;
@@ -27,14 +29,20 @@ public class ShamanCauldron extends AbstractImageEvent {
     private static final String[] DESCRIPTIONS = eventStrings.DESCRIPTIONS;
     private static final String[] OPTIONS = eventStrings.OPTIONS;
     private CurrentScreen currentScreen;
+    private static final String BASE_IMG = TheActMod.assetPath("images/events/Cauldron.png");
+    private static final String BROWN_IMG = TheActMod.assetPath("images/events/Cauldron.png");
 
     public ShamanCauldron() {
-        super(NAME, DESCRIPTIONS[0], null);
-        imageEventText.setDialogOption(OPTIONS[0]);
+        super(NAME, DESCRIPTIONS[0], BASE_IMG);
         if(AbstractDungeon.player.hasAnyPotions()){ //if the player has at least 1 potion
+            imageEventText.setDialogOption(OPTIONS[0]);
             imageEventText.setDialogOption(OPTIONS[1]);
+            imageEventText.setDialogOption(OPTIONS[2]);
         }
-        imageEventText.setDialogOption(OPTIONS[2]);
+        else {
+            imageEventText.setDialogOption(OPTIONS[0]);
+            imageEventText.setDialogOption(OPTIONS[2]);
+        }
         this.currentScreen = CurrentScreen.MAIN;
     }
 
@@ -47,16 +55,22 @@ public class ShamanCauldron extends AbstractImageEvent {
                         AbstractDungeon.actionManager.addToBottom(new LoseHPAction(AbstractDungeon.player, AbstractDungeon.player, 8));
                         CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.LOW, ScreenShake.ShakeDur.SHORT, false);
                         CardCrawlGame.sound.play("BLUNT_FAST");
+                        AbstractDungeon.player.damage(new DamageInfo(null, 8));
                         imageEventText.updateBodyText(DESCRIPTIONS[1]);
                         imageEventText.updateDialogOption(0, OPTIONS[3]);
                         imageEventText.clearRemainingOptions();
                         currentScreen = CurrentScreen.BROWNBREWFILL;
+                        imageEventText.loadImage(TheActMod.assetPath("images/events/CauldronBrown.png"));
                         break;
                     case 1: //Lose all potions and fill all slots
+                        for (int x = 0; x < AbstractDungeon.player.potionSlots; x++){
+                            AbstractDungeon.player.potions.set(x, new PotionSlot(x));
+                        }
                         imageEventText.updateBodyText(DESCRIPTIONS[2]);
                         imageEventText.updateDialogOption(0, OPTIONS[3]);
                         imageEventText.clearRemainingOptions();
                         currentScreen = CurrentScreen.BROWNBREWREPLACE;
+                        imageEventText.loadImage(TheActMod.assetPath("images/events/CauldronBrown.png"));
                         break;
                     case 2: //nope out
                         imageEventText.clearRemainingOptions();
@@ -67,17 +81,30 @@ public class ShamanCauldron extends AbstractImageEvent {
                 }
                 break;
             case BROWNBREWFILL: //fill
-                    fillPotionSlots();
-                    imageEventText.updateBodyText(DESCRIPTIONS[4]);
-                    imageEventText.updateDialogOption(0, OPTIONS[4]);
-                    currentScreen = CurrentScreen.COMPLETE;
-                    break;
-            case BROWNBREWREPLACE: //same as BROWNBREWFILL but you replace instead
-                    replacePotions();
-                    imageEventText.updateBodyText(DESCRIPTIONS[4]);
-                    imageEventText.updateDialogOption(0, OPTIONS[4]);
-                    currentScreen = CurrentScreen.COMPLETE;
-                    break;
+
+                int rando2 = (int)(Math.random() * (6) + 1); //1-6
+                for (int x = 0; x < AbstractDungeon.player.potionSlots; x++){
+                    AbstractPotion p = this.getJunglePotion(rando2);
+                    AbstractDungeon.player.obtainPotion(p);
+                }
+
+                imageEventText.updateBodyText(DESCRIPTIONS[4]);
+                imageEventText.updateDialogOption(0, OPTIONS[4]);
+                currentScreen = CurrentScreen.END;
+                break;
+            case BROWNBREWREPLACE:
+
+                int rando = (int)(Math.random() * (6) + 1); //1-6
+                for (int x = 0; x < AbstractDungeon.player.potionSlots; x++){
+                    AbstractPotion p = this.getJunglePotion(rando);
+                    AbstractDungeon.player.obtainPotion(p);
+                }
+
+                imageEventText.updateBodyText(DESCRIPTIONS[4]);
+                imageEventText.updateDialogOption(0, OPTIONS[4]);
+                currentScreen = CurrentScreen.END;
+
+                break;
             case COMPLETE: //done here
                 imageEventText.updateBodyText(DESCRIPTIONS[4]);
                 imageEventText.updateDialogOption(0, OPTIONS[4]);
@@ -89,21 +116,44 @@ public class ShamanCauldron extends AbstractImageEvent {
         }
     }
 
-    //TODO: Fix this and make it replace all potion slots with the random jungle potion
+
     private static void replacePotions(){
-        for (int x = 0; x <AbstractDungeon.player.potionSlots; x++){
-            AbstractPotion p = returnRandomPotion(PotionRarityEnum.JUNGLE, false);
-            //AbstractDungeon.player.potions.replaceAll(p); // how i do this :(
+        for (int x = 0; x < AbstractDungeon.player.potionSlots; x++){
+            AbstractDungeon.player.potions.set(x, new PotionSlot(x));
         }
     }
 
-    //TODO: Fix this and make it fill all empty potion slots with the random jungle potion
-    private static void fillPotionSlots(){
+    private void fillPotionSlots(){
+        AbstractDungeon.actionManager.addToBottom(new LoseHPAction(AbstractDungeon.player, AbstractDungeon.player, 10));
 
-        AbstractPotion p = returnRandomPotion(PotionRarityEnum.JUNGLE, false);
-        //for each empty potion slot do this...
-        AbstractDungeon.actionManager.addToBottom(new ObtainPotionAction(p));
+        AbstractPotion p = getJunglePotion((int)(Math.random() * (6) + 1));
 
+        int availableSlots = AbstractDungeon.player.potionSlots;
+        for (int x = 0; x < availableSlots; x++){
+            AbstractDungeon.actionManager.addToBottom(new LoseHPAction(AbstractDungeon.player, AbstractDungeon.player, 1));
+            AbstractDungeon.actionManager.addToBottom(new ObtainPotionAction(p));
+        }
+    }
+
+    private AbstractPotion getJunglePotion(int number){
+        AbstractDungeon.actionManager.addToBottom(new LoseHPAction(AbstractDungeon.player, AbstractDungeon.player, 3));
+
+        switch (number){
+            case 1:
+                return new Antidote();
+            case 2:
+                return new HauntedGourd();
+            case 3:
+                return new JungleJuice();
+            case 4:
+                return new SneckoExtract();
+            case 5:
+                return new TwistedElixir();
+            case 6:
+                return new SpyderVenom();
+            default:
+                return new TwistedElixir();
+        }
     }
 
 
