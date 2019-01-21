121ec63861220c6a2ab70eda6f780dc46a82b2cc
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-20 21:53:28 -0800

    get fUCKED PATCH

--- src/main/java/theAct/patches/JungleMusicPatchTemp.java
+++ src/main/java/theAct/patches/JungleMusicPatchTemp.java
@@ -5,27 +5,29 @@ import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.badlogic.gdx.audio.*;
 import theAct.TheActMod;
 
-@SpirePatch(clz = TempMusic.class, method = "getSong")
+@SpirePatch(
+        clz = TempMusic.class,
+        method = "getSong")
 public class JungleMusicPatchTemp {
 
     @SpirePostfixPatch
-    public static Music Postfix(Music __result, TempMusic __instance, String key) {
+    public static SpireReturn<Music> Prefix(TempMusic __instance, String key) {
         TheActMod.logger.info("Music patch Temp hit");
         switch (key) {
             case "JUNGLEELITE": {
-                return MainMusic.newMusic(TheActMod.assetPath("audio/music/jungle_elite.ogg"));
+                return SpireReturn.Return(MainMusic.newMusic(TheActMod.assetPath("audio/music/jungle_elite.ogg")));
             }
             case "BOSSTOTEM": {
-                return MainMusic.newMusic(TheActMod.assetPath("audio/music/boss_totem.ogg"));
+                return SpireReturn.Return(MainMusic.newMusic(TheActMod.assetPath("audio/music/boss_totem.ogg")));
             }
             case "BOSSSPIDER": {
-                return MainMusic.newMusic(TheActMod.assetPath("audio/music/boss_spider.ogg"));
+                return SpireReturn.Return(MainMusic.newMusic(TheActMod.assetPath("audio/music/boss_spider.ogg")));
             }
             case "BOSSMUSHROOM": {
-                return MainMusic.newMusic(TheActMod.assetPath("audio/music/boss_mushroom.ogg"));
+                return SpireReturn.Return(MainMusic.newMusic(TheActMod.assetPath("audio/music/boss_mushroom.ogg")));
             }
             default: {
-                return __result;
+                return SpireReturn.Continue();
             }
         }
 
