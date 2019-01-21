599e6381c90c42534287324ca5cef717a7f1bbfa
Kiooeht <kiooeht@gmail.com>
2019-01-20 16:03:27 -0800

    Fix Fragile Eggs power using name instead of ID

--- src/main/java/theAct/powers/FragileEggsPower.java
+++ src/main/java/theAct/powers/FragileEggsPower.java
@@ -11,25 +11,25 @@ import theAct.monsters.SpyderBoss.SpyderBoss;
 import theAct.powers.abstracts.Power;
 
 public class FragileEggsPower extends Power {
-	public static final String NAME = "FragileEggs";
-	public static final String powerID = TheActMod.makeID(NAME);
-	private static final PowerStrings strings = CardCrawlGame.languagePack.getPowerStrings(NAME);
+	public static final String POWER_ID = TheActMod.makeID("FragileEggs");
+	private static final PowerStrings strings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+	public static final String NAME = strings.NAME;
 	
 	private int reset = 0;
 
 	public FragileEggsPower(AbstractCreature owner, int amount) {
+		this.ID = POWER_ID;
+		this.name = strings.NAME;
 		this.owner = owner;
 		this.amount = amount;
 		this.reset = amount;
 		this.type = PowerType.BUFF;
-		this.name = strings.NAME;
 		this.setImage(NAME + "84.png", NAME + "32.png");
-		this.ID = powerID;
 		this.updateDescription();
 	}
 
 	public void updateDescription() {
-		this.description =	strings.DESCRIPTIONS[0] + reset + strings.DESCRIPTIONS[1] + amount + strings.DESCRIPTIONS[2];
+		this.description = strings.DESCRIPTIONS[0] + reset + strings.DESCRIPTIONS[1] + amount + strings.DESCRIPTIONS[2];
 	}	
     
     @Override
